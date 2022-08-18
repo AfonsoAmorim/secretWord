538569f6d5cbc50f289d9bb7ecf4ecf7037a6b05
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-08-18 16:11:47 -0300

    first commit

---
+++ projectWord/secret/src/data/words.js
@@ -0,0 +1,8 @@
+export const wordList = {
+    carro: ["Motor", "Porta", "Capô","Pneu", "Antena"],
+    fruta: ["Banana", "Maça", "Mamão"],
+    corpo: ["Braço", "Perna", "Cabeça", "Olhos"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Processador"],
+    programação: ["Linguagem", "Java", "React"],
+    alimento: ["Arroz", "Feijão", "Limão"],
+};
