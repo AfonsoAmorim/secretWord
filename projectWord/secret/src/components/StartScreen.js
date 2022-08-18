293d965c38d7c93c90724e809a69f1d5d4fe7a0d
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-08-18 16:24:54 -0300

    adicionando componentes

---
+++ projectWord/secret/src/components/StartScreen.js
@@ -0,0 +1,14 @@
+import "./StartScreen.css";
+
+const StartScreen = () =>{
+    return(
+        <div className="start">
+            <h1>Secret Word 007</h1>
+            <p>Clique no botão para começar.</p>
+            <button>Começar o jogo</button>
+        </div>
+    )
+}
+
+
+export default StartScreen;
